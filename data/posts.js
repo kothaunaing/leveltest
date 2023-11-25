81cd83d3eaf48c9ec762a186720bcd00b9d2cc94
kothaunaing <141340019+kothaunaing@users.noreply.github.com>
2023-11-26 00:21:01 +0630

    Add files via upload

---
+++ data/posts.js
@@ -0,0 +1,12 @@
+export const posts = [
+  {
+    text: 'SEA မှကျောင်းသားများအားတွေ့ရစဉ်',
+    day: '2 days ago',
+    images: [
+      'image1.jpg',
+      'image2.jpg',
+      'image3.jpg',
+      'image4.jpg',
+    ],
+  }
+];
--- scripts/pages.js
+++ scripts/pages.js
@@ -1,20 +1,90 @@
-export function homeHTML(){
+import { posts } from "../data/posts.js";
+
+export function homeHTML() {
+  let html = '';
+
+  posts.forEach((post) => {
+    const { text, day, images } = post;
+
+    html += `
+    <div class="home-container">
+    <div class="posts">
+      <div class="post-top-section">
+        <img class="sea-icon" src="images/logo square.jpg">
+        <div>
+          <div class="sea-name">Star Eduaction Academy (SEA)</div>
+          <div class="day-counter">
+            ${day}
+          </div>
+        </div>
+      </div>
+      <div class="post-middle-section">
+        ${text}
+      </div>
+    ${imageHTML(images)}
+    </div>
+  </div>
+    `;
+  });
+
   document.querySelector('.title')
     .style.display = 'none';
   document.querySelector('.app-display')
-    .innerHTML = 'Home HTML';
+    .innerHTML = html;
+
+  document.querySelectorAll('.post-image')
+    .forEach((element) => {
+      element.addEventListener('click', () => {
+        showImageViewer();
+        document.querySelector('.current-image')
+          .src = element.src;
+      });
+    });
+
+    document.querySelector('.close-image')
+      .addEventListener('click', () => {
+        hideImageViewer();
+      });
 }
 
-export function aboutHTML(){
-  document.querySelector('.title')
-    .style.display = 'none';
-  document.querySelector('.app-display')
-    .innerHTML = 'About HTML';
+const imageViewer = document.querySelector('.image-viewer');
+function showImageViewer() {
+  imageViewer.style.display = 'flex';
 }
 
-export function contactHTML(){
+function hideImageViewer() {
+  imageViewer.style.display = 'none';
+}
+
+export function aboutHTML() {
   document.querySelector('.title')
     .style.display = 'none';
   document.querySelector('.app-display')
-    .innerHTML = 'Contact HTML';
+    .innerHTML = 'Working on this feature';
+}
+
+export function contactHTML() {
+  document.querySelector('.title')
+    .style.display = 'none';
+  document.querySelector('.app-display')
+    .innerHTML = 'Working on this feature';
+}
+
+function imageHTML(images) {
+  let html = '';
+
+  images.forEach((image) => {
+    html += `
+      <img class="post-image" src="images/posts/${image}">
+    `;
+  });
+
+  if (images.length === 0) {
+    html = '';
+  }
+
+  return `
+  <div class="post-bottom-section">
+  ${html}
+ </div>`;
 }
